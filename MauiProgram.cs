4aea9da7d94084e988b2ff8e19ca9a164f12caff
Sham5iona <gom26@abv.bg>
2024-06-07 17:34:47 +0300

    This is the start up file in which one of the things I have done is to register the classes like the view, viewmodel and the model as services

---
+++ MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using VignetteSystem.Model.Interfaces;
+using VignetteSystem.Model.Services;
+using VignetteSystem.ViewModel;
+using VignetteSystem.Views;
+
+namespace VignetteSystem
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            builder.Services.AddTransient<ICheckVignette, BulgarianVignetteChecking>();
+            builder.Services.AddSingleton<HttpClient>();
+            builder.Services.AddTransient<BulgarianVignetteSystemViewModel>();
+            builder.Services.AddTransient<BulgarianVignetteSystemPage>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
